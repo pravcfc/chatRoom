7ef0cac5182229f6ae1badbd517ccad7973e96d1
praveen <praveen.beatle@gmail.com>
2015-05-26 15:03:47 +0300

    deployment changes to port

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var mime  = require('mime');
 
 var chatServer = require('./lib/chatServer');
 
+var port = process.env.PORT || 3000;
+
 var cache = {};
 
 
@@ -23,8 +25,8 @@ var server = http.createServer(function(request, response) {
   serveStatic(response, cache, absPath);
 });
 
-server.listen(3000, function() {
-  console.log("Server listening on port 3000.");
+server.listen(port, function() {
+  console.log("Server listening on port " + port + '.');
 });
 
 chatServer.listen(server);
